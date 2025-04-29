2ee797c231da0af9ea1d2d3ae46827d8aab4ca09
Kegnar <mitriy.exe@gmail.com>
2025-04-29 08:33:14 +0400

    доделал 4 задачу

---
+++ Task_4/Grade.cs
@@ -0,0 +1,8 @@
+﻿namespace Task_4;
+
+public class Grade
+{
+    public int StudentId { get; set; }
+    public string Subject { get; set; }
+    public char LetterGrade { get; set; }
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using Task_4;
+
+var students = new List<Student>
+{
+    new Student { Id = 1, Name = "Alice" },
+    new Student { Id = 2, Name = "Bob" }
+};
+
+var grades = new List<Grade>
+{
+    new Grade { StudentId = 1, Subject = "Math", LetterGrade = 'A' },
+    new Grade { StudentId = 2, Subject = "Math", LetterGrade = 'B' },
+    new Grade { StudentId = 1, Subject = "Science", LetterGrade = 'A' }
+};
+
+var studentGrades = from student in students    
+        // пробегаем по students
+    join grade in grades
+        on student.Id equals grade.StudentId
+    select new
+    {
+        student.Id,
+        student.Name,
+        grade.Subject,
+        grade.LetterGrade
+    };
+
+// Вывод результата
+foreach (var item in studentGrades)
+{
+    Console.WriteLine($"{item.Id} - {item.Name} - {item.Subject}: {item.LetterGrade}");
+}
