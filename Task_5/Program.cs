5bfe0072ee3e9fde33150ec1b30d7d4b66a6edf1
Dmitriy Morozov <mitriy.exe@gmail.com>
2025-04-29 12:10:34 +0400

    Final

---
+++ Task_5/Program.cs
@@ -0,0 +1,11 @@
+﻿using Task_5;
+
+var orders = new List<Order>
+{
+    new Order { Amount = 150.00m },
+    new Order { Amount = 200.00m },
+    new Order { Amount = 75.00m }
+};
+var ammount = orders.Sum(o => o.Amount);
+
+Console.WriteLine($"Сумма: {ammount}");
